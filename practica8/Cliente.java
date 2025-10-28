05b89b774102ca19cb1814536600bee481d4f404
Marco Quispe Flores <mquispeflores@unsa.edu.pe>
2025-10-28 18:08:12 -0500

    Agregando en cliente un contadorID y un get para obtener su ID

--- practica8/Cliente.java
+++ practica8/Cliente.java
@@ -1,20 +1,35 @@
 public class Cliente {
+    private static int contadorId = 1;
+    private int id;
     private String nombre;
     private String telefono;
     private String correo;
 
     public Cliente(String nombre, String telefono, String correo) {
+        this.id = contadorId++;
         this.nombre = nombre;
         this.telefono = telefono;
         this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
     @Override
     public String toString() {
-        return "Cliente: " + nombre + " | Tel: " + telefono + " | Correo: " + correo;
+        return "ID: " + id + " | Cliente: " + nombre + " | Tel: " + telefono + " | Correo: " + correo;
     }
 }
